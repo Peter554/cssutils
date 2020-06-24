957766e834ba59a6e1aa47619f403eaaf508066f
Peter Byfield <byfield554@gmail.com>
2020-06-24 17:14:41 +0200

    WIP

--- generate.js
+++ generate.js
@@ -5,10 +5,26 @@ const entries = Object.entries
 const generate = (config) => {
     let css = ''
 
-    for (const [k, v] of entries(config.generate)) {
-        for (const [k2, v2] of entries(config.variables[v.from])) {
-            css += `${v.alias ? v.alias : k}-${k2} { ${k}: ${v2}; }\n\n`
+    css += ':root {\n'
+    for (const [k1, v1] of entries(config.variables)) {
+        for (const [k2, v2] of entries(v1)) {
+            css += `    --${k1}-${k2}: ${v2};\n`
         }
+    }
+    css += '}\n\n'
+
+    for (const [k1, v1] of entries(config.generate)) {
+        for (const [k2, v2] of entries(config.variables[v1.from])) {
+            css += `${v1.alias ? v1.alias : k1}-${k2} { ${k1}: ${v2}; }\n`
+            if (v1.rotations) {
+                ;['left', 'right', 'top', 'bottom'].forEach((rotation) => {
+                    css += `${
+                        v1.alias ? v1.alias : k1
+                    }-${rotation}-${k2} { ${k1}-${rotation}: ${v2}; }\n`
+                })
+            }
+        }
+        css += '\n'
     }
 
     return css
