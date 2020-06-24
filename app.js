9f7855de435a1a8e7cabbfabe28f8d01ca4b0a8a
Peter Byfield <byfield554@gmail.com>
2020-06-24 15:34:10 +0200

    WIP

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
 #!/usr/bin/env node
 
-const { existsSync } = require('fs')
-const { resolve } = require('path')
+const { existsSync, readFileSync, writeFileSync } = require('fs')
+const { resolve, dirname } = require('path')
 
 const { program } = require('commander')
 const chalk = require('chalk')
+const YAML = require('yaml')
+const { mkdir } = require('shelljs')
 
 const log = console.log
-
 const error = chalk.bold.red
 
 program
     .name('cssutils')
-    .arguments('<inpath> <outpath>')
-    .action((inpath, outpath) => {
-        inpath = resolve(inpath)
-        if (!existsSync(inpath)) {
-            log(error(`${inpath} does not exist`))
+    .description('A CSS utility class generator.')
+    .arguments('<config> <output>')
+    .action((config, output) => {
+        config = resolve(config)
+        output = resolve(output)
+
+        if (!existsSync(config)) {
+            log(error(`${config} does not exist`))
             return
         }
+
+        config = YAML.parse(readFileSync(config, 'utf8'))
+
+        css = 'css'
+
+        mkdir('-p', dirname(output))
+        writeFileSync(output, css)
     })
 
 program.parse(process.argv)
